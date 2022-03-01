47ee6104e3d465b1522e29428341afc8a8eb9ef8
Korenak <andrewkorenak@hotmail.com>
2022-03-01 18:42:08 -0500

    add and delete added and functional.

--- korenak_andrew_python2/dbinit.py
+++ korenak_andrew_python2/dbinit.py
@@ -2,10 +2,13 @@ import os
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from forms import AddForm, DelForm
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mysecretkey'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -29,7 +32,7 @@ class Puppy(db.Model):
         if self.owner:
             return f"Puppy name is {self.name} and owner is {self.owner.name}"
         else:
-            return f"Puppy name is {self.name} and has no owner assigned yet."
+            return f"{self.name} has no owner assigned yet."
 
 
 class Owner(db.Model):
@@ -43,3 +46,63 @@ class Owner(db.Model):
     def __init__(self,name,puppy_id):
         self.name = name
         self.puppy_id = puppy_id
+
+    def __repr__(self):
+        return f"{self.name} \n"
+
+db.create_all()
+##############VIEWS###############################################
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_pup():
+    form = AddForm()
+
+    if form.validate_on_submit():
+        # we have 2 required input fields
+        name = form.name.data
+        name2 = form.name2.data
+        # Add puppies
+        new_pup = Puppy(name)
+        db.session.add(new_pup)
+        db.session.commit()
+        #the aobve ensures the puppy is assigned an id
+        # before the below attempts to attach an owner to new_pup.id
+        puppy_id= new_pup.id
+        new_owner= Owner(name2,puppy_id)
+        db.session.add(new_owner)
+        db.session.commit()
+
+        return redirect(url_for('list_pup'))
+
+    return render_template('add_pup.html', form=form)
+
+
+@app.route('/list')
+def list_pup():
+    # Grab a list of puppies from database.
+    puppies = Puppy.query.all()
+
+    return render_template('list_pup.html', puppies=puppies)
+
+
+@app.route('/delete', methods=['GET', 'POST'])
+def del_pup():
+    form = DelForm()
+
+    if form.validate_on_submit():
+        id = form.id.data
+        pup = Puppy.query.get(id)
+        db.session.delete(pup)
+        db.session.commit()
+
+        return redirect(url_for('list_pup'))
+    return render_template('delete_pup.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ korenak_andrew_python2/forms.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template,request
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField
+from wtforms.validators import DataRequired
+
+class AddForm(FlaskForm):
+    name = StringField('Puppy Name:',validators=[DataRequired()])
+    name2= StringField('Owner Name',validators=[DataRequired()])
+    submit = SubmitField('Add Puppy')
+
+
+class DelForm(FlaskForm):
+    id = IntegerField('Puppy ID:')
+    submit = SubmitField('Remove Puppy')
