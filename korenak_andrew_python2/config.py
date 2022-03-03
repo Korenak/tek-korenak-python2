57f4522a108b0b9520cb3be90b10509325927f88
Korenak <andrewkorenak@hotmail.com>
2022-03-03 16:10:39 -0500

    Created Config.py. Some fields MUST be filled before running project.

---
+++ korenak_andrew_python2/config.py
@@ -0,0 +1,9 @@
+### Config file to change connection stings for MySQL
+# change the values after = to set your information.
+# username = 'asdfasdf'   DO NOT FORGET '' or "" to make sure it is string.
+username = ''
+# password = 'asdfasdfa'
+password = ''
+# including port number ex localhost:3306 is optional
+host = 'localhost'
+schema = 'sakila'
